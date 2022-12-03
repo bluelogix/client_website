f2b187e1642e48711db1e47b6c3f1334e007a134
bluelogix <bluelogix010@gmail.com>
2022-12-03 14:40:51 -0800

    matchMedia working

--- script.js
+++ script.js
@@ -16,27 +16,29 @@ burger.addEventListener('click', () => {
 })
 
 const videos = gsap.utils.toArray('.video');
-gsap.set(videos, {opacity: 0})
 
-videos.forEach((video) => {
-    ScrollTrigger.create({
-        trigger: video,
-        start: 'top center',
-        end: 'bottom center',
-        scrub: true,
-        onEnter: () => {
-            gsap.to(video, {opacity:1});
-            video.play();
-        },
-        onEnterBack: () => video.play(),
-        onLeave: () => video.pause(),
-        onLeaveBack: () => video.pause()
-    })
-})
+
+// videos.forEach((video) => {
+//     ScrollTrigger.create({
+//         trigger: video,
+//         start: 'top center',
+//         end: 'bottom center',
+//         scrub: true,
+//         onEnter: () => {
+//             gsap.to(video, {opacity:1});
+//             video.play();
+//         },
+//         onEnterBack: () => video.play(),
+//         onLeave: () => video.pause(),
+//         onLeaveBack: () => video.pause()
+//     })
+// })
 
 let mm = gsap.matchMedia();
-mm.add('min-width: 500px', () => {
-    videos.forEach((video) => {
+console.log(mm)
+mm.add('(min-width: 500px)', () => {
+gsap.set(videos, {opacity: 0})
+videos.forEach((video) => {
         ScrollTrigger.create({
             trigger: video,
             start: 'top center',
@@ -51,8 +53,8 @@ mm.add('min-width: 500px', () => {
             onLeaveBack: () => video.pause()
         })
     })
-    
-    })
+})
+
     
 
 
